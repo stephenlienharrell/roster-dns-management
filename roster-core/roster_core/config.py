54712451485ef94ac92c2b2ae332a7efa52ebb05
alwayschooseford <alwayschooseford@dc4e0264-a2cd-11de-8f97-31926e5e0c50>
2009-10-19 19:02:02 +0000

    Made rosterd switch to a username specified in the config file before running.

--- roster-core/roster_core/config.py
+++ roster-core/roster_core/config.py
@@ -72,6 +72,7 @@ class Config(object):
                    'passwd': 'str', 'database': 'str'},
                    'server': {'inf_renew_time': 'int', 'core_die_time': 'int',
                    'get_credentials_wait_increment': 'int',
+                   'run_as_username': 'str',
                    'server_killswitch': 'boolean', 'lock_file': 'str',
                    'ssl_key_file': 'str', 'ssl_cert_file': 'str'},
                    'credentials': {'authentication_method': 'str',
