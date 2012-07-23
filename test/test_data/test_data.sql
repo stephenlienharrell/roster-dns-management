ef46b6f5c94197bf033220bbbe5cc49c60fc9ed6
j.s.pavlick <j.s.pavlick@dc4e0264-a2cd-11de-8f97-31926e5e0c50>
2012-07-23 17:26:47 +0000

    Fixing unittests. Every single unittest should run correctly now.

--- test/test_data/test_data.sql
+++ test/test_data/test_data.sql
@@ -19,11 +19,12 @@ INSERT INTO user_group_assignments
     (user_group_assignments_group_name, user_group_assignments_user_name)
   VALUES ('bio', 'shuey');
 
-# Create Zones and Zone Origins
+# Create Zones
 INSERT INTO zones (zone_name) VALUES ('cs.university.edu');
 INSERT INTO zones (zone_name) VALUES ('bio.university.edu');
 INSERT INTO zones (zone_name) VALUES ('eas.university.edu');
 
+# Create Zone Origins
 INSERT INTO zone_view_assignments 
     (zone_origin, zone_view_assignments_zone_type, zone_view_assignments_zone_name,
      zone_view_assignments_view_dependency, zone_options) 
@@ -64,6 +65,5 @@ INSERT INTO reverse_range_permissions
     (reverse_range_permissions_group_name, reverse_range_permissions_cidr_block,
      reverse_range_permissions_group_permission) 
   VALUES ('bio', '192.168.1.0/24', 'rw');
-
 
 # vi: set ai aw sw=2:
