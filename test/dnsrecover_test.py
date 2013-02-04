bac2b938fcb78d1bfae415dd8b6f9bbb0209862b
cooker52@gmail.com <cooker52@gmail.com@dc4e0264-a2cd-11de-8f97-31926e5e0c50>
2013-02-04 19:51:31 +0000

    Added test to dnsrecover_test to check that the duplicate record bug has been fixed.

--- test/dnsrecover_test.py
+++ test/dnsrecover_test.py
@@ -70,6 +70,7 @@ CREDFILE='%s/.dnscred' % os.getcwd()
 EXEC='../roster-config-manager/scripts/dnsrecover'
 TESTDIR = u'%s/unittest_dir/' % os.getcwd()
 BINDDIR = u'%s/test_data/named/' % os.getcwd()
+TEST_DNS_SERVER = u'localhost'
 SSH_USER = unicode(getpass.getuser())
 
 class StdOutStream():
@@ -260,6 +261,124 @@ class TestDnsRecover(unittest.TestCase):
     self.assertEqual(output.read(),
         'ERROR: An audit log ID must be specified to recover Roster.\n')
     output.close()
+
+  def testPreventDuplicateRecordBug(self):
+    for zone in self.core_instance.ListZones():
+        self.core_instance.RemoveZone(zone)
+    self.core_instance.MakeDnsServer(TEST_DNS_SERVER, SSH_USER, BINDDIR, TESTDIR)
+    self.core_instance.MakeDnsServerSet(u'set1')
+    self.core_instance.MakeDnsServerSetAssignments(TEST_DNS_SERVER, u'set1')
+    self.core_instance.MakeNamedConfGlobalOption(u'set1', u'options { pid-file "test_data/named.pid"; };')
+
+    self.core_instance.MakeView(u'test_view')
+    self.core_instance.MakeDnsServerSetViewAssignments(u'test_view', 1, u'set1')
+
+    self.core_instance.MakeZone(u'test_zone', u'master', u'university.lcl.', u'test_view')
+    self.core_instance.MakeRecord(u'soa', u'@', u'test_zone', {u'name_server': u'ns.university.lcl.', u'admin_email': '%s.university.lcl.' % USERNAME,
+        u'refresh_seconds': 5, u'expiry_seconds': 5, u'minimum_seconds': 5, u'retry_seconds': 5, u'serial_number': 999}, u'test_view')
+    self.core_instance.MakeRecord(u'ns', u'@', u'test_zone', {u'name_server': u'ns.university.lcl.'}, u'test_view')
+
+    # Extracted as ID 13
+    self.tree_exporter_instance.ExportAllBindTrees()
+
+    self.core_instance.MakeRecord(u'a', u'record02', u'test_zone', {u'assignment_ip': u'192.168.1.2'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'record03', u'test_zone', {u'assignment_ip': u'192.168.1.3'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'record04', u'test_zone', {u'assignment_ip': u'192.168.1.4'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'record05', u'test_zone', {u'assignment_ip': u'192.168.1.5'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'record06', u'test_zone', {u'assignment_ip': u'192.168.1.6'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'record07', u'test_zone', {u'assignment_ip': u'192.168.1.7'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'record08', u'test_zone', {u'assignment_ip': u'192.168.1.8'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'record09', u'test_zone', {u'assignment_ip': u'192.168.1.9'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'record10', u'test_zone', {u'assignment_ip': u'192.168.1.10'}, u'test_view')
+
+    # Extracted as ID 23
+    self.tree_exporter_instance.ExportAllBindTrees()
+
+    # Recover to audit id 19
+    output = os.popen('python %s --config-file %s -u %s -i 19 --auto-export' % (EXEC, 
+        CONFIG_FILE, USERNAME))
+    lines = output.read().split('\n')
+    output.close()
+
+    self.assertEqual(lines, ['Loading database from backup with ID 13',
+        'Replaying action with id 14: MakeRecord',
+        "with arguments: [u'a', u'record02', u'test_zone', {u'assignment_ip': u'192.168.1.2'}, u'test_view', None]",
+        'Replaying action with id 15: MakeRecord',
+        "with arguments: [u'a', u'record03', u'test_zone', {u'assignment_ip': u'192.168.1.3'}, u'test_view', None]",
+        'Replaying action with id 16: MakeRecord',
+        "with arguments: [u'a', u'record04', u'test_zone', {u'assignment_ip': u'192.168.1.4'}, u'test_view', None]",
+        'Replaying action with id 17: MakeRecord',
+        "with arguments: [u'a', u'record05', u'test_zone', {u'assignment_ip': u'192.168.1.5'}, u'test_view', None]",
+        'Replaying action with id 18: MakeRecord',
+        "with arguments: [u'a', u'record06', u'test_zone', {u'assignment_ip': u'192.168.1.6'}, u'test_view', None]",
+        '',
+        'Running dnstreeexport',
+        'dnstreeexport has completed successfully',
+        ''])
+
+    # Check to see if we only have the records we want now
+    records = self.core_instance.ListRecords(record_type=u'a')
+    self.assertEqual(records, [
+        {'target': u'record02', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': USERNAME, 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.2'},
+        {'target': u'record03', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': USERNAME, 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.3'},
+        {'target': u'record04', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': USERNAME, 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.4'},
+        {'target': u'record05', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': USERNAME, 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.5'},
+        {'target': u'record06', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': USERNAME, 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.6'}])
+    # Records 'record07-10' are no longer here.  Perfect, this is what we want.
+
+    # Making some more records
+    self.core_instance.MakeRecord(u'a', u'roster01', u'test_zone', {u'assignment_ip': u'192.168.2.1'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster02', u'test_zone', {u'assignment_ip': u'192.168.2.2'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster03', u'test_zone', {u'assignment_ip': u'192.168.2.3'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster04', u'test_zone', {u'assignment_ip': u'192.168.2.4'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster05', u'test_zone', {u'assignment_ip': u'192.168.2.5'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster06', u'test_zone', {u'assignment_ip': u'192.168.2.6'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster07', u'test_zone', {u'assignment_ip': u'192.168.2.7'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster08', u'test_zone', {u'assignment_ip': u'192.168.2.8'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster09', u'test_zone', {u'assignment_ip': u'192.168.2.9'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster10', u'test_zone', {u'assignment_ip': u'192.168.2.10'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster11', u'test_zone', {u'assignment_ip': u'192.168.2.11'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster12', u'test_zone', {u'assignment_ip': u'192.168.2.12'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster13', u'test_zone', {u'assignment_ip': u'192.168.2.13'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster14', u'test_zone', {u'assignment_ip': u'192.168.2.14'}, u'test_view')
+    self.core_instance.MakeRecord(u'a', u'roster15', u'test_zone', {u'assignment_ip': u'192.168.2.15'}, u'test_view')
+
+    # Only wanted the first 6 of those
+    # Recover to audit id 35
+    output = os.popen('python %s --config-file %s -u %s -i 35 --auto-export' % (EXEC, 
+        CONFIG_FILE, USERNAME))
+    lines = output.read().split('\n')
+    output.close()
+
+    # Confirm output (will change when dnsrecover changes)
+    self.assertEqual(lines, ['Loading database from backup with ID 29',
+        'Replaying action with id 30: MakeRecord',
+        "with arguments: [u'a', u'roster01', u'test_zone', {u'assignment_ip': u'192.168.2.1'}, u'test_view', None]",
+        'Replaying action with id 31: MakeRecord',
+        "with arguments: [u'a', u'roster02', u'test_zone', {u'assignment_ip': u'192.168.2.2'}, u'test_view', None]",
+        'Replaying action with id 32: MakeRecord',
+        "with arguments: [u'a', u'roster03', u'test_zone', {u'assignment_ip': u'192.168.2.3'}, u'test_view', None]",
+        'Replaying action with id 33: MakeRecord',
+        "with arguments: [u'a', u'roster04', u'test_zone', {u'assignment_ip': u'192.168.2.4'}, u'test_view', None]",
+        'Replaying action with id 34: MakeRecord',
+        "with arguments: [u'a', u'roster05', u'test_zone', {u'assignment_ip': u'192.168.2.5'}, u'test_view', None]",
+        '',
+        'Running dnstreeexport',
+        'dnstreeexport has completed successfully',
+        ''])
+
+    # Checking that proper records are in database (currently breaks)
+    records = self.core_instance.ListRecords(record_type=u'a')
+    self.assertEqual(records, [{'target': u'record02', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.2'},
+        {'target': u'record03', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.3'},
+        {'target': u'record04', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.4'},
+        {'target': u'record05', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.5'},
+        {'target': u'record06', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.1.6'},
+        {'target': u'roster01', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.2.1'},
+        {'target': u'roster02', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.2.2'},
+        {'target': u'roster03', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.2.3'},
+        {'target': u'roster04', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.2.4'},
+        {'target': u'roster05', 'ttl': 3600, 'record_type': u'a', 'view_name': u'test_view', 'last_user': u'sharrell', 'zone_name': u'test_zone', u'assignment_ip': u'192.168.2.5'}])
 
 if( __name__ == '__main__' ):
       unittest.main()
